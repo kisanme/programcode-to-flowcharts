208a4595e1b008758d0c28526b6d72b8d640514d
Nasik <nasik2ms@gmail.com>
2018-01-05 15:50:33 +0530

    Using phplex given in phply to generate tokens for php classes/files.

--- src/main.py
+++ src/main.py
@@ -1,2 +1,13 @@
-import lib.ply.lex as lex
-import lib.ply.yacc as yacc
+import lib.phplex as phplex
+
+def parse_through_lex(filepath):
+  data = open(filepath, 'r').read()
+  phplex.full_lexer.input(data)
+  return phplex.full_lexer
+
+
+lexemes = parse_through_lex('./php_test_files/BasicClass.php')
+# Tokenize
+for tok in lexemes:
+  print(tok.type)
+  print(tok.value)
---
+++ src/php_test_files/BasicClass.php
@@ -0,0 +1,15 @@
+<?php
+class BasicClass {
+
+	public function hello() {
+		echo ("printing hello");
+	}
+
+	public function helloConditional() {
+		if (1 == 1) {
+			echo ("printing hello");
+		} else {
+			echo ("HI");
+		}
+	}
+}
