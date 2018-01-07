9e79b6baac74b7c2e11cc9cfc81ae114bc55831a
Nasik <nasik2ms@gmail.com>
2018-01-07 16:29:24 +0530

    Flowchart Drawer completed. Drawing tests are done at src/main.py

--- src/drawer/drawer.py
+++ src/drawer/drawer.py
@@ -9,6 +9,8 @@ class Drawer(object):
 
   def initialize_drawing(self):
     self.gr = pygraphviz.AGraph(directed=True, strict=True, rankdir='LR')
+    self.gr.node_attr['shape'] = 'rectangle'
+    self.gr.add_node('Start', shape='ellipse')
 
   def add_process(self, process_name):
     self.gr.add_node(process_name, shape='rectangle')
@@ -16,8 +18,16 @@ class Drawer(object):
   def add_io(self, io_name):
     self.gr.add_node(io_name, shape='parallelogram')
 
-  def connect(self, from_node, to_node):
-    self.gr.add_edge(from_node, to_node)
+  def add_decision(self, decision_name):
+    self.gr.add_node(decision_name, shape='diamond')
+
+  def connect(self, from_node, to_node, connect_name=''):
+    self.gr.add_edge(from_node, to_node, label=connect_name)
+
+  def end(self, from_node):
+    end_name = 'End'
+    self.gr.add_node(end_name, shape='ellipse')
+    self.connect(from_node, end_name)
 
   def get_drawing(self):
     return self.gr
