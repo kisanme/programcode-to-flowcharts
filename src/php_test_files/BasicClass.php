af73e481f27c480cce17cd8cc20ffbf18e6da370
Nasik <nasik2ms@gmail.com>
2018-03-17 09:14:30 +0530

    Evaluation item scaffolding

--- src/php_test_files/BasicClass.php
+++ src/php_test_files/BasicClass.php
@@ -13,6 +13,26 @@ class BasicClass {
 			echo ("HI");
 		}
 	}
+
+	public function nestedIfEvaluation() {
+		if ($x < 2) {
+		    if ($x == 0) {
+                echo ("Zero test");
+		    }
+			echo ("1 or negative number");
+		} else {
+			echo ("positive numbers greater than 2");
+		}
+	}
+
+
+    public function whileEvaluation() {
+      $counter = 0;
+      while ($counter < 100) {
+        $some_array[] = $counter;
+        $counter ++;
+      }
+    }
 //
 //	public function switch_test () {
 //	    switch (contenttype) {
