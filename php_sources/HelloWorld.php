8927a27813c2911c449e4f83ae1cb7e0051fbf0d
Nasik <nasik2ms@gmail.com>
2018-04-04 12:53:55 +0530

    Demo items under constructions.

---
+++ php_sources/HelloWorld.php
@@ -0,0 +1,8 @@
+<?php
+
+class HelloWorld {
+  public function helloWorld()
+  {
+    echo ("Hello World");
+  }
+}
---
+++ php_sources/IfElse.php
@@ -0,0 +1,14 @@
+<?php
+
+class IfElse{
+  public function ifElse()
+  {
+    $x = 1;
+    if ($x==1) {
+      echo ("$x is greater than 0");
+    } else {
+      echo ("$x is is less than 1");
+    }
+    
+  }
+}
